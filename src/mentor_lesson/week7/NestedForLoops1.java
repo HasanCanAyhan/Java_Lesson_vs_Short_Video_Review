da49a16a7cce61d28e9b47cd3facda8fa0b41ba9
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-06-04 12:17:09 +0200

    NestedForLoops

---
+++ src/mentor_lesson/week7/NestedForLoops1.java
@@ -0,0 +1,27 @@
+package mentor_lesson.week7;
+
+public class NestedForLoops1 {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 5; i >= 1 ; i--) {
+
+            for (int j = 1;  j <= i  ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
+/*
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+ */

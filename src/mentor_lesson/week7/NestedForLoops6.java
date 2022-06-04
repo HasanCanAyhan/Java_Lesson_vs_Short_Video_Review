63b5f5b50298423ec257ac125f518dd56b192dd1
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-06-04 12:43:23 +0200

    NestedForLoops6

---
+++ src/mentor_lesson/week7/NestedForLoops6.java
@@ -0,0 +1,33 @@
+package mentor_lesson.week7;
+
+public class NestedForLoops6 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5 ; i++) {
+
+            for (int k = 1; k < i ; k++) {
+                System.out.print("* ");
+            }
+
+
+
+            for (int j = 6-i; j >= 1 ; j--) {
+                System.out.print(j+" ");
+            }
+
+
+            System.out.println();
+        }
+
+
+    }
+
+}
+/*
+5 4 3 2 1
+* 4 3 2 1
+* * 3 2 1
+* * * 2 1
+* * * * 1
+ */

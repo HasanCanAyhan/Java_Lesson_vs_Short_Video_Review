0f9ba1447fbe90b9beba184908b7c2b0ccc4c7e2
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-31 23:44:55 +0200

    JavaDev

--- src/mentor_lesson/week19/FirstUniqueCharacter_Stream.java
+++ src/mentor_lesson/week19/FirstUniqueCharacter_Stream.java
@@ -2,6 +2,9 @@ package mentor_lesson.week19;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FirstUniqueCharacter_Stream {
 
@@ -10,15 +13,25 @@ public class FirstUniqueCharacter_Stream {
         String str = "success";
         System.out.println( firstUniqueCharacter(str)  );
 
+        System.out.println("----------------------------");
+
+        System.out.println(solutionWithStream(str));
+
+        System.out.println("----------------------------");
+
+        System.out.println(solutionWithStream2(str));
+
     }
 
     private static Character firstUniqueCharacter(String str){
 
         Map<Character,Integer> map = new LinkedHashMap<>();
 
+
+        int count = 0;
         for (char eachCh : str.toCharArray()) {
 
-                map.put(eachCh, map.getOrDefault(eachCh,0)+1);
+            map.put(eachCh, map.getOrDefault(eachCh,0)+1);
 
         }
 
@@ -32,6 +45,52 @@ public class FirstUniqueCharacter_Stream {
 
 
         return ' ';
+    } // solution with map
+
+    private static char solutionWithStream(String str){
+
+        LinkedHashMap<Character,Integer> frequencyMap = new LinkedHashMap<>();
+        for (int i = 0; i < str.length() ; i++) {
+
+            char currentChar = str.charAt(i);
+            frequencyMap.put(currentChar, frequencyMap.getOrDefault(currentChar,0)+1);
+        }
+
+        Optional<Map.Entry<Character, Integer>> opt = frequencyMap.entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .findFirst();
+
+        if (opt.isPresent()){
+            return opt.get().getKey();
+        }
+
+        return ' ';
+
+    }
+
+    private static char solutionWithStream2(String str){
+
+        Map<Character,Integer> frequencyMap = new LinkedHashMap<>();
+        for (int i = 0; i < str.length() ; i++) {
+            char ch = str.charAt(i);
+            frequencyMap.put(ch, frequencyMap.getOrDefault(ch,0)+1);
+        }
+
+        return frequencyMap.keySet().stream().filter(ch -> frequencyMap.get(ch) == 1).findFirst().orElse(' ');
+
+
+    }
+
+    private static char solutionWithStream3(String str){ // difficult part
+
+        Map<Character,Long> frequencyMap = str.chars()
+                .mapToObj(i -> (char)i)
+                .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()));
+        return frequencyMap.entrySet().stream()
+                .filter(ch -> ch.getValue() == 1)
+                //.map(ch -> ch.getKey())
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(' ');
     }
 
 }

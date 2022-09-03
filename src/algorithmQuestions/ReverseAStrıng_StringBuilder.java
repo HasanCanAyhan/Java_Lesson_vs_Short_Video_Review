0ba0a62fe9e983fc131d6029a37d04f8c71da925
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-03 17:33:43 +0200

    JavaDev

--- src/algorithmQuestions/ReverseAStrıng_StringBuilder.java
+++ src/algorithmQuestions/ReverseAStrıng_StringBuilder.java
@@ -1,5 +1,5 @@
 package algorithmQuestions;
-
+//https://youtu.be/gy1uve2BwG8
 public class ReverseAStrıng_StringBuilder {
 
     public static void main(String[] args) {
@@ -9,6 +9,35 @@ public class ReverseAStrıng_StringBuilder {
         StringBuilder stringBuilder = new StringBuilder(str);
         StringBuilder reverse = stringBuilder.reverse();
         System.out.println("reverse = " + reverse);
+
+        System.out.println("------------------------------------------");
+
+        System.out.println(reverseWithStringBuilder(str));
+
+        System.out.println("------------------------------------------");
+
+        System.out.println(reverseManually(str));
+
     }
+
+    private static String reverseWithStringBuilder(String str) {
+
+        return new StringBuilder(str).reverse().toString();
+
+    }
+
+    private static String reverseManually(String str){
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = str.length() -1 ; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+
+
+        return sb.toString();
+
+    }
+
 
 }

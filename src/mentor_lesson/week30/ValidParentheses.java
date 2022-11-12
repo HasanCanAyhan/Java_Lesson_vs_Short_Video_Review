0c7e5f4c19a78921f3c587eaf19b6812efd77a06
Hasan Can Ayhan <ayhanhasancan55@gmail.com>
2022-11-13 00:03:50 +0100

    JavaDev

--- src/mentor_lesson/week30/ValidParentheses.java
+++ src/mentor_lesson/week30/ValidParentheses.java
@@ -1,8 +1,40 @@
 package mentor_lesson.week30;
 
+import java.util.Stack;
+
 public class ValidParentheses {
 
     public static void main(String[] args) {
+
+        String str = ")()())";
+
+        String s = str.replace(" ", "");
+
+        Stack<Character> characters = new Stack<>();
+
+        if (s.isEmpty()) return;
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch != '('){
+                characters.push(ch);
+            }
+
+        }
+
+        int c = 1;
+        int i = 0;
+        while (!characters.isEmpty() && i <= s.length()-1) {
+
+            if (s.charAt(i) != ')') {
+                characters.pop();
+                c = 2 * c;
+            }
+
+            i++;
+        }
+
+        System.out.println(c);
 
 
 

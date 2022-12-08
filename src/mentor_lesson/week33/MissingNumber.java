fb040c738e20fca79f5ff8722e1c468eeca499d5
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-08 11:23:26 +0100

    JavaDev

--- src/mentor_lesson/week33/MissingNumber.java
+++ src/mentor_lesson/week33/MissingNumber.java
@@ -10,7 +10,7 @@ public class MissingNumber {
         //int[] arr1 = {9,6,4,2,3,5,7,0,1}; // ->> 8
         //int[] arr1 = {3,0,1}; //-->> 2
         int[] arr1 = {0,1};
-        //int n = arr1.length;
+
 
         System.out.println(findMissingNumber(arr1));
 
@@ -19,15 +19,32 @@ public class MissingNumber {
     public static int findMissingNumber(int[] arr){
 
         int n = arr.length;
-        int sum =  ( n * (n+1) )/ 2;
+        int sum =  ( n * (n+1) )/ 2; // 1 + 2+ 3 = 6; // 3
 
         int sum_arr = 0;
         for (int i = 0; i < arr.length ; i++) {
-             sum_arr += arr[i];
+             sum_arr += arr[i]; // 1
         }
 
-        return sum - sum_arr;
+        return sum - sum_arr; // 6 - 4 = 2
 
     }
+
+    public static int withSorting(int[] nums){
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length ; i++) {
+            if (nums[i] != i){
+                return i;
+            }
+        }
+
+        return nums.length;
+
+    }
+
+
+
 
 }
